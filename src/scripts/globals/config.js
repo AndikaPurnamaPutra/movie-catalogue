5085e999d5e285d6a00613764fdab992323f8706
andika purnama putra <andikapratamaputra1210@gmail.com>
2023-11-08 00:46:43 +0700

    first commit

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+	KEY: "b152a2a5f7dcf20dd4912e67909c5d0a",
+	BASE_URL: "https://api.themoviedb.org/3/",
+	BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+	DEFAULT_LANGUAGE: "en-us",
+};
+
+export default CONFIG;
